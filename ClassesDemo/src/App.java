c4e21cfd958382e11faaac67741f1e68c26727ad
YinonTsadik <yinonts1500@gmail.com>
2022-11-27 17:46:30 +0200

    Files organization

--- ClassesDemo/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Controller controller = new Controller(30, 7);
-        controller.optimization();
-        System.out.println(controller.getAllDays());
-    }
-}
