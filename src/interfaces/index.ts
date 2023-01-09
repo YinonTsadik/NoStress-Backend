0f70afdc75c3e55e8a2ec531cb28ce2df03a4f39
YinonTsadik <yinonts1500@gmail.com>
2023-01-10 00:21:39 +0200

    Deleted interfaces

--- src/interfaces/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './interfaces/userInterfaces'
-export * from './interfaces/calendarInterfaces'
-export * from './interfaces/taskInterfaces'
-export * from './interfaces/constraintsInterfaces'
