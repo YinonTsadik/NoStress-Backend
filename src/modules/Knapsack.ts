b13d6e3a4d8b510df955f74020c761a69aaf0717
YinonTsadik <yinonts1500@gmail.com>
2023-05-16 19:19:20 +0300

    Documented the Knapsack & Optimization functions

--- src/modules/Knapsack.ts
+++ src/modules/Knapsack.ts
@@ -10,19 +10,29 @@ export default class Knapsack {
         this.capacity = capacity
     }
 
+    /**
+     * Solves the Knapsack problem.
+     * Returns a Solution object containing the selected tasks,
+     * the total value of the selected tasks, and the total number of hours.
+     */
     public solve(): Solution {
+        // Get the number of tasks and the capacity of the knapsack
         const numOfTasks = this.tasks.length
         const capacity = this.capacity
 
+        // Create a matrix to store the maximum values for different subproblems
         const matrix = new Array<Array<number>>(numOfTasks + 1)
         for (let i = 0; i < matrix.length; i++)
             matrix[i] = new Array<number>(capacity + 1).fill(0)
 
+        // Fill in the matrix using dynamic programming
         for (let i = 1; i <= numOfTasks; i++) {
             for (let j = 0; j <= capacity; j++) {
                 if (this.tasks[i - 1].getWorkHours > j) {
+                    // If the current task cannot fit in the remaining capacity, skip it
                     matrix[i][j] = matrix[i - 1][j]
                 } else {
+                    // Consider the maximum value between including and excluding the current task
                     matrix[i][j] = Math.max(
                         matrix[i - 1][j],
                         matrix[i - 1][j - this.tasks[i - 1].getWorkHours] +
@@ -32,6 +42,7 @@ export default class Knapsack {
             }
         }
 
+        // Determine the selected tasks based on the matrix
         const tasksSolution = new Array<Task>()
         let numOfHours = 0
         let result = matrix[numOfTasks][capacity]
@@ -39,14 +50,17 @@ export default class Knapsack {
 
         for (let i = numOfTasks; i > 0 && result > 0; i--) {
             if (matrix[i - 1][j] !== result) {
+                // If the value in the matrix changed, it means the task was selected
                 tasksSolution.push(this.tasks[i - 1])
                 numOfHours += this.tasks[i - 1].getWorkHours
 
+                // Update the remaining capacity and total value
                 result -= this.tasks[i - 1].getValue
                 j -= this.tasks[i - 1].getWorkHours
             }
         }
 
+        // Return the solution
         return new Solution(tasksSolution, matrix[numOfTasks][capacity], numOfHours)
     }
 }
--- src/modules/Manager.ts
+++ src/modules/Manager.ts
@@ -15,15 +15,26 @@ export default class Manager {
         this.allTasks = new Array<Task>()
     }
 
-    public async generateManager() {
+    /**
+     * Initializes a manager by populating the `allDays` and `allTasks` properties.
+     * Retrieves calendar data and tasks from the database and initializes corresponding objects.
+     */
+    public async initManager() {
+        // Retrieve calendar data from the database
         const calendar = await db.getCalendar(this.calendarID)
 
+        // Generate all days between the calendar's start and end dates
         this.allDays = Day.generateCalendar(calendar.startDate, calendar.endDate)
+
+        // Apply scheduling constraints to each day based on the calendar ID
         this.allDays.forEach((day: Day) =>
             day.constraintsScheduling(this.calendarID)
         )
 
+        // Retrieve tasks associated with the calendar from the database
         const tasks = await db.getCalendarTasks(this.calendarID)
+
+        // Create Task objects for each retrieved task and add them to the `allTasks` array
         tasks.forEach((task: TaskInterface) => {
             const newTask = new Task(
                 task.id,
@@ -31,35 +42,55 @@ export default class Manager {
                 task.deadline,
                 task.workHours
             )
+
+            // Update the task's details using the current date
             newTask.updateDetails(new Date())
+
+            // Add the task to the `allTasks` array
             this.allTasks.push(newTask)
         })
     }
 
+    /**
+     * Optimizes the scheduling of tasks within the calendar.
+     * Deletes previously scheduled tasks associated with the calendar from the database.
+     * Uses the Knapsack algorithm to find the best task selection for each day and updates the schedule accordingly.
+     */
     public async optimize() {
+        // Delete previously scheduled tasks associated with the calendar from the database
         db.deleteCalendarScheduledTasks(this.calendarID)
 
+        // Iterate through each day in the calendar
         this.allDays.forEach((day: Day) => {
             const options = new Array<Task>()
             const availableHours = day.getAvailableHours
 
+            // Iterate through each task in the list of all tasks
             this.allTasks.forEach((task: Task) => {
                 if (!task.getFullyScheduled) {
+                    // Create a temporary copy of the task and update its details based on the current day
                     const tempTask = Task.copyConstructor(task)
                     tempTask.updateDetails(day.getDate)
 
+                    // Skip the task if its value is negative after the update
                     if (tempTask.getValue < 0) return
+
                     if (task.getWorkHours <= availableHours) {
+                        // If the task can fit within the available hours, add it as an option
                         options.push(tempTask)
                     } else {
+                        // If the task needs to be split to fit within the available hours, add the split version as an option
                         options.push(tempTask.splitTask(availableHours))
                     }
                 }
             })
 
+            // Use the Knapsack algorithm to find the best task selection for the day
+            // day.tasksScheduling(this.calendarID, daySolution.getTasks)
             const dayKnapsack = new Knapsack(options, availableHours)
             const daySolution = dayKnapsack.solve()
 
+            // Ensure that the scheduled solution does not exceed the available hours for the day
             if (daySolution.getHours > availableHours) {
                 throw new Error('Knapsack Error')
             }
@@ -70,18 +101,21 @@ export default class Manager {
             console.log('hours:')
             console.log(daySolution.getHours)
 
-            // day.tasksScheduling(this.calendarID, daySolution.getTasks)
+            // Update the day's schedule and available hours based on the solution
             day.setAvailableHours = day.getAvailableHours - daySolution.getHours
             day.setTotalValue = daySolution.getValue
 
+            // Update the scheduling status of the tasks in the solution and split tasks if necessary
             for (let i = 0; i < daySolution.getTasks.length; i++) {
                 const solutionTask = daySolution.getTasks[i]
                 for (let j = 0; j < this.allTasks.length; j++) {
                     if (this.allTasks[j].getID === solutionTask.getID) {
                         const originalTask = this.allTasks[j]
                         if (originalTask.getWorkHours <= availableHours) {
+                            // Mark the original task as fully scheduled
                             originalTask.setFullyScheduled = true
                         } else {
+                            // Split the original task and replace it with the split version
                             this.allTasks.splice(
                                 j,
                                 1,
