3fb064b9094152dc34c0b5f9352e59928ac7e1c7
YinonTsadik <yinonts1500@gmail.com>
2023-05-22 22:23:07 +0300

    Yanir's Knapsack algorithm

--- src/modules/Knapsack.ts
+++ src/modules/Knapsack.ts
@@ -21,27 +21,49 @@ export default class Knapsack {
         const numOfTasks = this.tasks.length
         const capacity = this.capacity
 
+        console.log(JSON.stringify(this.tasks))
         // Create a matrix to store the maximum values for different subproblems
         const matrix = new Array<Array<number>>(numOfTasks + 1)
         for (let i = 0; i < matrix.length; i++)
             matrix[i] = new Array<number>(capacity + 1).fill(0)
 
+        console.log(matrix)
         // Fill in the matrix using dynamic programming
         for (let i = 1; i <= numOfTasks; i++) {
-            for (let j = 0; j <= capacity; j++) {
-                if (this.tasks[i - 1].getWorkHours > j) {
+            console.log(`Reading getWorkHours of ${i - 1}...`)
+            const workHours = this.tasks[i - 1].getWorkHours
+            const value = this.tasks[i - 1].getValue
+            for (let j = 1; j < capacity + 1; j++) {
+                console.log(`J IS ${j}`)
+                if (workHours > j) {
                     // If the current task cannot fit in the remaining capacity, skip it
                     matrix[i][j] = matrix[i - 1][j]
+                    if (Number.isNaN(matrix[i][j])) {
+                        console.log(
+                            `IF: ${i},${j}. ${JSON.stringify(this.tasks[i - 1])}`
+                        )
+                    }
                 } else {
                     // Consider the maximum value between including and excluding the current task
                     matrix[i][j] = Math.max(
                         matrix[i - 1][j],
-                        matrix[i - 1][j - this.tasks[i - 1].getWorkHours] +
-                            this.tasks[i - 1].getValue
+                        matrix[i - 1][j - workHours] + value
                     )
+                    if (Number.isNaN(matrix[i][j])) {
+                        console.log(
+                            `ELSE: ${i},${j}. ${JSON.stringify(this.tasks[i - 1])}`
+                        )
+                        console.log('......................................')
+                        console.log(matrix[i - 1][j])
+                        console.log(matrix[i - 1][j - workHours] + value)
+                    }
                 }
             }
         }
+
+        console.log('result and matrix:')
+        console.log(matrix[numOfTasks][capacity])
+        console.log(matrix)
 
         // Determine the selected tasks based on the matrix
         const tasksSolution = new Array<Task>()
@@ -49,18 +71,20 @@ export default class Knapsack {
         let result = matrix[numOfTasks][capacity]
         let j = capacity
 
-        for (let i = numOfTasks; i > 0 && result > 0; i--) {
-            if (matrix[i - 1][j] !== result) {
+        for (let i = numOfTasks; i > 0; i--) {
+            if (matrix[i][j] !== matrix[i - 1][j]) {
+                console.log(`Adding task ${this.tasks[i - 1].getID}`)
                 // If the value in the matrix changed, it means the task was selected
                 tasksSolution.push(this.tasks[i - 1])
                 numOfHours += this.tasks[i - 1].getWorkHours
 
                 // Update the remaining capacity and total value
-                result -= this.tasks[i - 1].getValue
+                //result -= this.tasks[i - 1].getValue
                 j -= this.tasks[i - 1].getWorkHours
             }
         }
 
+        console.log('NOH: ' + numOfHours)
         // Return the solution
         return new Solution(tasksSolution, matrix[numOfTasks][capacity], numOfHours)
     }
